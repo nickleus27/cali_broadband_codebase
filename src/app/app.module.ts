d20e335874873cab31e1bf69326de79ef217dd71
nickleus27 <nickleus27@gmail.com>
2023-01-25 16:50:35 -0800

    toolbar and sidenav list items

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,8 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {MatSidenavModule} from '@angular/material/sidenav';
-import { HomePageComponent } from './components/home-page/home-page.component'
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { HomePageComponent } from './components/home-page/home-page.component';
 
 @NgModule({
   declarations: [
@@ -18,6 +23,11 @@ import { HomePageComponent } from './components/home-page/home-page.component'
     AppRoutingModule,
     BrowserAnimationsModule,
     MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatListModule,
+    MatMenuModule,
+    MatExpansionModule,
     FormsModule,
     ReactiveFormsModule
   ],
