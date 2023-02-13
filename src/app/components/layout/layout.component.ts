bad048250a543e308b2dc17742b32838ccbd6e40
nickleus27 <nickleus27@gmail.com>
2023-02-13 14:53:24 -0800

    added home button to top toolbar

--- src/app/components/layout/layout.component.ts
+++ src/app/components/layout/layout.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-layout',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class LayoutComponent {
 
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router
+  ) { }
+
+  goHome(): void {
+    this.router.navigate([''], { relativeTo: this.route });
+  }
 }
