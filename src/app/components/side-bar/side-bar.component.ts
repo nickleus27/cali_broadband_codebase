9f33e95d8072d191ec3c621c3646381aaf849316
nickleus27 <nickleus27@gmail.com>
2023-02-19 21:45:35 -0800

    state service, reformating side-bar comonent

--- src/app/components/side-bar/side-bar.component.ts
+++ src/app/components/side-bar/side-bar.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnChanges, OnInit } from '@angular/core';
 import { ComponentDataService } from 'src/app/services/component-data/component-data.service';
 import { GetDataService } from 'src/app/services/get-data/get-data.service';
+import { SidebarStateService } from 'src/app/services/sidebar-state/sidebar-state.service';
 import { GraphOptions } from './GraphOptions';
 
 @Component({
@@ -18,40 +19,41 @@ export class SideBarComponent implements OnInit, OnChanges {
 
   constructor(
     private getDataService: GetDataService,
-    public compDataService: ComponentDataService
-    ) {
+    public compDataService: ComponentDataService,
+    private sidebarState: SidebarStateService
+  ) {
     this.roundData = {};
-    this.graphOptions = { 
+    this.graphOptions = {
       comparison: false,
-      rounds:['round14', 'round15', 'round16'],
-      graphs: ['graph1', 'graph2', 'graph3'], graphSelected:'graph1',
+      rounds: ['round14', 'round15', 'round16'],
+      graphs: ['graph1', 'graph2', 'graph3'], graphSelected: 'graph1',
       graph1: {},
       graph2: {},
       graph3: {}
     };
     this.optionsSelected = this.graphOptions.graph1;
-    this.compDataService.displaySideNavComp.subscribe(flag =>
-      {
-        this.comparison = flag;
-        this.graphOptions.comparison = flag;
-        if (this.comparison) {
-          this.compDataService.updateNumCompGraphs();
-          let index:number = this.compDataService.numCompGraphs;
-          this.graphOptions.graphSelected = this.graphOptions.graphs[index];
-          this.optionsSelected = this.graphOptions[this.graphOptions.graphSelected as keyof GraphOptions];
-          this.ngOnChanges();
-        }
-      });
+    this.compDataService.displaySideNavComp.subscribe(flag => {
+      this.comparison = flag;
+      this.graphOptions.comparison = flag;
+      if (this.comparison) {
+        this.compDataService.updateNumCompGraphs();
+        let index: number = this.compDataService.numCompGraphs;
+        this.graphOptions.graphSelected = this.graphOptions.graphs[index];
+        this.optionsSelected = this.graphOptions[this.graphOptions.graphSelected as keyof GraphOptions];
+        this.sidebarState.state = this.graphOptions.graphSelected;
+        this.ngOnChanges();
+      }
+    });
     this.compDataService.displayPhoneFlag
-      .subscribe(flag =>
-        { this.optionsSelected.phoneSelected = flag;
-          this.ngOnChanges();
-        }
+      .subscribe(flag => {
+        this.optionsSelected.phoneSelected = flag;
+        this.ngOnChanges();
+      }
       );
   }
 
   ngOnInit(): void {
-   this.getDataService.getRound('round14')
+    this.getDataService.getRound('round14')
       .subscribe((result) => {
         this.roundData['round14'] = result;
       });
@@ -64,6 +66,7 @@ export class SideBarComponent implements OnInit, OnChanges {
         this.roundData['round16'] = result;
       });
   }
+
   ngOnChanges(): void {
     this.optionsSelected = this.graphOptions[this.graphOptions.graphSelected as keyof GraphOptions];
     if (this.graphOptions.roundSelected) {
@@ -91,10 +94,13 @@ export class SideBarComponent implements OnInit, OnChanges {
       }
     }
     if (this.optionsSelected.serverSelected) {
+      this.sidebarState.acceptingState(this.graphOptions.graphSelected);
       this.getDataService.setGraphParams(this.graphOptions);
-      this.compDataService.updateGraphButtonFlag(false);
-      if (this.compDataService.numCompGraphs < 2) {
-        this.compDataService.updateCompButtonFlag(false);
+      if (this.sidebarState.accepting) {
+        this.compDataService.updateGraphButtonFlag(false);
+        if (this.compDataService.numCompGraphs < 2) {
+          this.compDataService.updateCompButtonFlag(false);
+        }
       }
     }
   }
---
+++ src/app/services/sidebar-state/sidebar-state.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SidebarStateService {
+
+  private _sidebarState: any;
+
+  constructor() { 
+    this._sidebarState = {
+      state: 'graph1',
+      graph1: false,
+      graph2: false,
+      graph3: false,
+      };
+    }
+
+  get accepting(): boolean {
+    return this._sidebarState[this._sidebarState.state];
+  }
+
+  set state(state: string) {
+    this._sidebarState.state = state;
+  }
+
+  acceptingState(state: string) {
+    this._sidebarState[state] = true;
+  }
+}
