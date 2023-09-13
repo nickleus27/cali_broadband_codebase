8012e3137a51788ddd3424521c3c27f07e7c3e0d
nickleus27 <nickleus27@gmail.com>
2023-09-13 16:29:42 -0700

    new dir: line-county-graph (component)

---
+++ src/app/components/line-county-graph/line-county-graph.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-line-county-graph',
+  templateUrl: './line-county-graph.component.html',
+  styleUrls: ['./line-county-graph.component.css']
+})
+export class LineCountyGraphComponent {
+
+}
